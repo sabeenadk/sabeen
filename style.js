8ec328b66a104d5226207fa88b5e2247608b6d6b
roshan adhikari <adksabin779@gmail.com>
2024-09-18 09:43:06 +0545

    first commit

---
+++ style.js
@@ -0,0 +1,10 @@
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+div.innerHTML="<h1> Hello World ! </h1>";
+
+let hi = document.getElementById("hi");
+let bye = document.getElementById("bye");
+const first =document.getElementsByClassName("first");
+
+hi.style.backgroundColor="red";
